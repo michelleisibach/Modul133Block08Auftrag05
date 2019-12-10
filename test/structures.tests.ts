de1ad60bead186ed975c20e974b565c4163426bc
Michel Leisibach <michel_leisibach@sluz.ch>
2019-12-10 20:38:20 +0100

    Refactoring in structures.tests.ts

--- test/structures.tests.ts
+++ test/structures.tests.ts
@@ -3,27 +3,27 @@ import { Queue } from "../lib/Queue";
 import { Stack } from "../lib/Stack";
 
 describe("Data structures", () => {
-    describe("Queue", () => {
+    describe("QueueTests", () => {
         it("Empty Queue, Size 0", () => {
             const testee = new Queue<number>();
             const result = testee.size();
             expect(result).to.equal(0);
         })
 
-        it("unempty queue returns isEmpty=false", () => {
+        it("Unempty Queue, isEmpty=false", () => {
             const testee = new Queue<number>();
             testee.enqueue(15);
             const result = testee.isEmpty();
             expect(result).to.be.false;
         })
 
-        it("empty queue returns isEmpty=true", () => {
+        it("Empty Queue, isEmpty=true", () => {
             const testee = new Queue<number>();
             const result = testee.isEmpty();
             expect(result).to.be.true;
         })
 
-        it("enqueue item increases size", () => {
+        it("Enqueue Item, increases size", () => {
             const testee = new Queue<number>();;
             testee.enqueue(11);
             testee.enqueue(22);
@@ -32,7 +32,7 @@ describe("Data structures", () => {
             expect(result).to.equal(3);
         })
 
-        it("peek item returns first added value", () => {
+        it("Peek Item, first added value", () => {
             const testee = new Queue<number>();
             testee.enqueue(11);
             testee.enqueue(22);
@@ -41,121 +41,100 @@ describe("Data structures", () => {
             expect(result).to.equal(11);
         })
 
-        it("peek item returns does not affect size", () => {
+        it("Peek Item, does not affect size", () => {
             const testee = new Queue<number>();
             testee.enqueue(11);
             testee.enqueue(22);
             testee.enqueue(33);
             testee.peek();
             const result = testee.size();
-
             expect(result).to.equal(3);
         })
 
-        it("poll item returns first added value", () => {
+        it("Poll Item, first added value", () => {
             const testee = new Queue<number>();
-
             testee.enqueue(11);
             testee.enqueue(22);
             testee.enqueue(33);
             const result = testee.poll();
-
             expect(result).to.equal(11);
         })
 
-        it("poll item returns does affect size", () => {
+        it("Poll Item, does affect size", () => {
             const testee = new Queue<number>();
-
             testee.enqueue(11);
             testee.enqueue(22);
             testee.enqueue(33);
             testee.poll();
             const result = testee.size();
-
             expect(result).to.equal(2);
         })
     })
 
-    describe("Stack", () => {
-        it("empty stack has zero size", () => {
+    describe("StackTests", () => {
+        it("Empty Stack, Size 0", () => {
             const testee = new Stack<number>();
-
             const result = testee.size();
-
             expect(result).to.equal(0);
         })
 
-        it("non-empty stack returns false isEmpty", () => {
+        it("UnEmpty Stack, isEmpty=false", () => {
             const testee = new Stack<number>();
-
             testee.enqueue(15);
             const result = testee.isEmpty();
-
             expect(result).to.be.false;
         })
 
-        it("empty stack returns true isEmpty", () => {
+        it("Empty Stack, isEmpty=true", () => {
             const testee = new Stack<number>();
-
             const result = testee.isEmpty();
-
             expect(result).to.be.true;
         })
 
-        it("enqueue item increases size", () => {
+        it("Enqueue Item, increases size", () => {
             const testee = new Stack<number>();
-
             testee.enqueue(11);
             testee.enqueue(22);
             testee.enqueue(33);
             const result = testee.size();
-
             expect(result).to.equal(3);
         })
 
-        it("peek item returns last added value", () => {
+        it("Peek Item, last added value", () => {
             const testee = new Stack<number>();
-
             testee.enqueue(11);
             testee.enqueue(22);
             testee.enqueue(33);
             const result = testee.peek();
-
             expect(result).to.equal(33);
         })
 
-        it("peek item returns doesnt affect size", () => {
+        it("Peek Item, doesnt affect size", () => {
             const testee = new Stack<number>();
-
             testee.enqueue(11);
             testee.enqueue(22);
             testee.enqueue(33);
             testee.peek();
             const result = testee.size();
-
             expect(result).to.equal(3);
         })
 
-        it("poll item returns last added value", () => {
+        it("Poll Item, last added value", () => {
             const testee = new Stack<number>();
-
             testee.enqueue(11);
             testee.enqueue(22);
             testee.enqueue(33);
             const result = testee.poll();
-
             expect(result).to.equal(33);
         })
 
-        it("poll item returns affects size", () => {
+        it("Poll Item, affects size", () => {
             const testee = new Stack<number>();
-
             testee.enqueue(11);
             testee.enqueue(22);
             testee.enqueue(33);
             testee.poll();
             const result = testee.size();
-
             expect(result).to.equal(2);
         })
     })
